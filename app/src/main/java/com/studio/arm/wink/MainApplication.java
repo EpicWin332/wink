c7618f9a6361e3eb771b5c9ebfe9b11b49e470d4
Alexander <alex27594@gmail.com>
2015-10-31 15:41:40 +0300

    VK SDK

---
+++ app/src/main/java/com/studio/arm/wink/MainApplication.java
@@ -0,0 +1,33 @@
+package com.studio.arm.wink;
+
+import android.app.Application;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
+
+/**
+ * Created by alexander on 27.10.15.
+ */
+public class MainApplication extends Application {
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(VKAccessToken oldToken, VKAccessToken newToken) {
+            if (newToken == null) {
+                // VkAccessToken is invalid
+            }
+        }
+
+
+    };
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        vkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
+
+    }
+
+}
